bbfe1dacc067a0beb9ce461171516aa10b8c0b3a
Meraj Nouredini <merajnooredini@gmail.com>
2013-03-04 15:47:27 +0330

    User registeration module

--- meraj_book_sharing/UserReg/models.py
+++ meraj_book_sharing/UserReg/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     # This field is required.
     user = models.OneToOneField(User)
-    
+    #maybe a field must be define here!    
 
     # Other fields here
     accepted_eula = models.BooleanField()
